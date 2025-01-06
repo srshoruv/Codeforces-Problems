2e02e8f987a1540db3eface5d0cf25efeac1d5b8
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-01-06 21:43:51 +0600

    solved some problems

---
+++ candiesAndTwoSisters.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin >> testCase;
+
+    while (testCase--)
+    {
+        int n, ans = 0;
+        cin >> n;
+
+        if (n % 2 == 0)
+        {
+            ans = (n / 2) - 1;
+        }
+        else
+        {
+            ans = n / 2;
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
---
+++ iWannaBeTheGuy.c++
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int maxLevel;
+    cin >> maxLevel;
+
+    vector<int> vec1;
+    vector<int> vec2;
+
+    for (int i = 0; i < maxLevel; i++)
+    {
+        cin >> vec1[i];
+        cin >> vec2[i];
+    }
+    return 0;
+}
