bf8e7f78615632b847744f41bab65100a0ebbb8a
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-01-23 03:29:19 +0600

    solved

---
+++ Div 3 - 19 Jan/fibonacciness.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int testCase;
+    cin >> testCase;
+
+    while (testCase--)
+    {
+        vector<int> vec(4);
+
+        for (int i=0; i<4; i++)
+        {
+           cin >> vec[i];
+           cout << vec[i];
+        }
+        
+
+    }
+
+    return 0;
+}
