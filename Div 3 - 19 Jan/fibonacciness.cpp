e885b0a4afa1e85f8ebc41e1e1203b9c9cef938b
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-01-24 21:06:48 +0600

    Update fibonacciness.cpp

--- Div 3 - 19 Jan/fibonacciness.cpp
+++ Div 3 - 19 Jan/fibonacciness.cpp
@@ -9,11 +9,17 @@ int main()
     while (testCase--)
     {
         vector<int> vec(4);
+        int first, second;
 
         for (int i=0; i<4; i++)
         {
            cin >> vec[i];
-           cout << vec[i];
+        }
+
+        for (int i=0; i<4; i++)
+        {
+           first = vec[i];
+           second = vec[i+1];
         }
         
 
