8b9f2dcd13a83919486f2f37bf00ab1ce82d7984
Shahidur Rahman <166603336+srshoruv@users.noreply.github.com>
2025-04-27 13:02:16 +0600

    solved a problem

---
+++ cloudberryJam.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+int main() {
+    fastio;
+
+    int testCase;
+    cin >> testCase;
+
+    while (testCase--)
+    {
+
+    	// Your code here
+    	int n;
+    	cin >> n;
+    	cout << n * 2 << endl;
+
+    }
+    
+    return 0;
+}
